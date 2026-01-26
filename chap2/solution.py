dfa421edefa61122f2f3cd5a0ae4a91c69cd2767
Cyonecoder <khassal.mohamed1@gmail.com>
2026-01-26 21:49:47 +0100

    commit chap2

---
+++ chap2/solution.py
@@ -0,0 +1,10 @@
+name ='Albert Einstein'
+quote =" once said, “A person who never made a mistake never tried anything new” "
+print(f"hello {name}")
+print(name.upper())
+print(name.lower())
+print(name.title())
+print(name, quote)
+famous_person=' Albert Einstein '
+message =" once said, “A person who never made a \nmistake never tried anything new”"
+print(famous_person.rstrip().lstrip(), message)
