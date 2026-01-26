274f2f3362c7540710aba186c3ce33c09e45bc5f
Cyonecoder <khassal.mohamed1@gmail.com>
2026-01-26 21:59:51 +0100

    chap 3 start

---
+++ chapter3/solution.py
@@ -0,0 +1,9 @@
+names= ['p1','p2','p3']
+
+for i in names:
+    print('hello',i)
+
+transport_items=['ferrari spider', 'private jet', 'boat']
+
+for i in transport_items:
+    print("i would like to own a",i)
