5e8585623348777500ab9ec5900c54cd3b10d64b
Mohamed Khassal <khassal.mohamed1@gmail.com>
2026-01-29 21:49:35 +0100

    finish with inputs

--- chapter7/inputfunc.py
+++ chapter7/inputfunc.py
@@ -1,56 +1,56 @@
-# message = input("tell me something , and iw ill repeat it back to you :")
-# print(message)
-# name = input("please enter your name: ")
-# print(f"\n hello {name}")
+message = input("tell me something , and iw ill repeat it back to you :")
+print(message)
+name = input("please enter your name: ")
+print(f"\n hello {name}")
 
-# prompt = "if you tell us who you afre ,m we can personalize the message you see."
-# prompt += "\nWhat is your first name? "
-# name = input(prompt)
-# print(f"\nHello, {name}")
-# age = input("how old are you? ")
-# age = int(age)
-# print(age >= 18)
+prompt = "if you tell us who you afre ,m we can personalize the message you see."
+prompt += "\nWhat is your first name? "
+name = input(prompt)
+print(f"\nHello, {name}")
+age = input("how old are you? ")
+age = int(age)
+print(age >= 18)
 
-# rental_car = input("What kind of car do you want to rent? ")
+rental_car = input("What kind of car do you want to rent? ")
 
-# print(f"\n let me find you a {rental_car}")
+print(f"\n let me find you a {rental_car}")
 
-# seating = input("How many people are in the dinner group")
+seating = input("How many people are in the dinner group")
 
-# seating_number = int(seating)
-# if seating_number > 8:
-#     print("You have to wait for a table.")
-# else:
-#     print("table is ready")
-# number_enter = input("enter a number")
+seating_number = int(seating)
+if seating_number > 8:
+    print("You have to wait for a table.")
+else:
+    print("table is ready")
+number_enter = input("enter a number")
 
-# number_entred_int = int(number_enter)
-# if number_entred_int % 10 == 0:
-#     print(f"{number_enter} is a multiple of 10")
-# else:
-#     print(f"{number_enter} is a not a multiple of 10 ")
-# enter = ""
-# while enter != "quit":
-#     msg = input("enter a topping: ")
-#     enter = msg
-#     if msg != "quit":
+number_entred_int = int(number_enter)
+if number_entred_int % 10 == 0:
+    print(f"{number_enter} is a multiple of 10")
+else:
+    print(f"{number_enter} is a not a multiple of 10 ")
+enter = ""
+while enter != "quit":
+    msg = input("enter a topping: ")
+    enter = msg
+    if msg != "quit":
 
-#         print(f"\n you enter topping {msg} to your pizza")
+        print(f"\n you enter topping {msg} to your pizza")
 
-# age = ""
-# while age != "quit":
-#     age = input("enter your Age :")
-#     if age == "quit":
-#         break
-#     int_age = int(age)
+age = ""
+while age != "quit":
+    age = input("enter your Age :")
+    if age == "quit":
+        break
+    int_age = int(age)
 
-#     if int_age < 3:
-#         print("Ticket price is free")
-#     elif int_age >= 3 and int_age <= 12:
-#         print("Ticket price is 10$")
+    if int_age < 3:
+        print("Ticket price is free")
+    elif int_age >= 3 and int_age <= 12:
+        print("Ticket price is 10$")
 
-#     else:
-#         print("Ticket price is 15$")
+    else:
+        print("Ticket price is 15$")
 unconfirmed_users = ["alice", "brian", "candace"]
 confirmed_users = []
 while unconfirmed_users:
@@ -63,3 +63,35 @@ while "cat" in pets:
     pets.remove("cat")
 
 print(pets)
+
+sandwich_orders = [
+    "pastrami",
+    "cheese burger",
+    "chicken Burger",
+    "pastrami",
+    "double cheese Burger",
+    "osta chicken",
+    "pastrami",
+]
+print("the deli run out of pastrami")
+while "pastrami" in sandwich_orders:
+    sandwich_orders.remove("pastrami")
+print(sandwich_orders)
+finished_sandwich = []
+
+while sandwich_orders:
+    current_sandwich = sandwich_orders.pop()
+    print(f"I made your {current_sandwich.title()} sandwich")
+    finished_sandwich.append(current_sandwich)
+print(finished_sandwich)
+
+dream_vacation = {}
+flag = True
+while flag:
+    name = input("enter your name :")
+    vacation_destination = input("enter your vacation dream destination :")
+    dream_vacation[name] = vacation_destination
+    keep_looping = input("do you wanna keep looping Yes/no: ")
+    if keep_looping.lower() == "no":
+        flag = False
+print(dream_vacation)
