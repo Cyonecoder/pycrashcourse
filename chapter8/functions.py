0e7ecccebbc7e7b3b03ecfa9c7727689a7115ab2
Cyonecoder <khassal.mohamed1@gmail.com>
2026-01-31 21:58:22 +0100

    function done

--- chapter8/functions.py
+++ chapter8/functions.py
@@ -1,3 +1,5 @@
+from pizza import make_pizza
+
 # def greet_user():
 #     """Display a simple greeting"""
 #     print("Hello!")
@@ -108,11 +110,11 @@ def city_country(city, country):
 # print(city_countryname)
 
 
-def make_album(artist_name, album_title, songsNb=None):
-    album_artist = {"artist": artist_name, "album": album_title}
-    if songsNb:
-        album_artist["songsNb"] = songsNb
-    return album_artist
+# def make_album(artist_name, album_title, songsNb=None):
+#     album_artist = {"artist": artist_name, "album": album_title}
+#     if songsNb:
+#         album_artist["songsNb"] = songsNb
+#     return album_artist
 
 
 # album_artist = make_album("toto", "mghayar")
@@ -120,12 +122,133 @@ def make_album(artist_name, album_title, songsNb=None):
 # album_artist = make_album("toto", "camelon", 9)
 # print(album_artist)
 
-while True:
-    name = input("enter name: ")
-    if name == "q":
-        break
-    album = input("enter album: ")
-    if album == "q":
-        break
-    album_artist = make_album(name, album)
-    print(album_artist)
+# while True:
+#     name = input("enter name: ")
+#     if name == "q":
+#         break
+#     album = input("enter album: ")
+#     if album == "q":
+#         break
+#     album_artist = make_album(name, album)
+#     print(album_artist)
+
+
+# def greet_users(names):
+#     for name in names:
+#         msg = f"Hello, {name.title()}"
+#         print(msg)
+
+
+# usernames = ["hannah", "ty", "margot"]
+# greet_users(usernames)
+
+# unprinted_designs = ["phone cases", "robot pendant", "dodecahedron"]
+# completed_models = []
+# while unprinted_designs:
+#     current_design = unprinted_designs.pop()
+#     print(f"Printing mnodel: {current_design}")
+#     completed_models.append(current_design)
+
+
+# print("\n the following models have been printed:")
+# for completed_model in completed_models:
+#     print(completed_model)
+# def print_models(unprinted_designs, completed_models):
+#     while unprinted_designs:
+#         current_design = unprinted_designs.pop()
+#         print(f"Printing mnodel: {current_design}")
+#         completed_models.append(current_design)
+
+
+# def show_completed_models(completed_models):
+#     print("\nThe following models have been printed")
+#     for completed_model in completed_models:
+#         print(completed_model)
+
+
+# unprinted_designs = ["phone cases", "robot pendant", "dodecahedron"]
+# completed_models = []
+# print_models(unprinted_designs[:], completed_models)
+# show_completed_models(completed_models)
+# print(unprinted_designs)
+
+
+# def make_pizza(*toppings):
+#     print(toppings)
+
+
+# def make_pizza(*toppings):
+#     print("\nMaking a pizza with the following toppings:")
+#     for topping in toppings:
+#         print(f"- {topping}")
+
+
+# make_pizza("peperoni")
+# make_pizza("mushrooms", "green peppers", "extra cheese")
+
+
+# def make_pizza(size, *toppings):
+#     print(f"\nMaking a {size}-inch pizza with the following toppings:")
+#     for topping in toppings:
+#         print(f"- {topping}")
+
+
+# make_pizza(16, "peperoni")
+# make_pizza(12, "mushrooms", "green peppers", "extra cheese")
+
+
+# def build_profile(first, last, **user_info):
+#     user_info["first_name"] = first
+#     user_info["last_name"] = last
+#     return user_info
+
+
+# user_profile = build_profile(
+#     "albert", "einstein", location="princeton", field="physics"
+# )
+# print(user_profile)
+# pizza.make_pizza(16, "peperoni")
+# pizza.make_pizza(12, "mushrooms", "green peppers", "extra cheese")
+# make_pizza(16, "peperoni")
+# make_pizza(12, "mushrooms", "green peppers", "extra cheese")
+
+messages = ["text1", "text2", "text3"]
+sent_messages = []
+
+
+def show_messages(messages, sent_messages):
+    while messages:
+        current_message = messages.pop()
+        sent_messages.append(current_message)
+    print(f"message is{messages},\nsent_messages is {sent_messages}")
+
+
+show_messages(messages[:], sent_messages)
+
+
+def make_sandwich(*sandwich_add_ons):
+    for item in sandwich_add_ons:
+        print(item)
+
+
+make_sandwich("hq", "sd", "sdde")
+
+
+def build_profile(first, last, **user_info):
+    user_info["first_name"] = first
+    user_info["last_name"] = last
+    return user_info
+
+
+me = build_profile("simo", "khass", tall=180, weight=70, city="Sale")
+print(me)
+
+
+def build_car(manufacturer, model, **car_info):
+    car_info["manufacturer"] = manufacturer
+    car_info["model"] = model
+    return car_info
+
+
+car = build_car("BMW", "M4", style="coupe", year=2026, price=67_000)
+print(car)
