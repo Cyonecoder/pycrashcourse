68388fedbf0db678b6b31ec4d877c5a0216b12b3
Mohamed Khassal <khassal.mohamed1@gmail.com>
2026-02-03 12:06:54 +0100

    exercice 9th finished

--- chapter9/classes.py
+++ chapter9/classes.py
@@ -44,3 +44,32 @@ salome = Restaurant("Pizza Napolitano", "Italian")
 salome.describe_restaurant()
 salome = Restaurant("El Marrakech", "Morocan")
 salome.describe_restaurant()
+
+
+class User:
+    def __init__(self, first_name, last_name, email, date_of_birth, location):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.date_of_birth = date_of_birth
+        self.location = location
+
+    def describe_user(self):
+        print(
+            f"User: {self.first_name+" "+self.last_name} born in {self.date_of_birth} located in {self.location}"
+        )
+
+    def greet_user(self):
+        print(f"Hello, {self.first_name}")
+
+
+user1 = User("hamid", "smires", "hamid.smires@gg.co", 1990, "lalaland")
+user2 = User("karima", "kiki", "kiki.ma@fo.com", 1990, "llulucity")
+user3 = User("nada", "3bi9a", "nana.3bi@dd.ha", 1990, "hozlofik")
+
+user1.describe_user()
+user1.greet_user()
+user2.describe_user()
+user2.greet_user()
+user3.describe_user()
+user3.greet_user()
