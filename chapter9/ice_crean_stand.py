7c0bef0e272ee1159ebf937950551bfdd0b9f462
Mohamed Khassal <khassal.mohamed1@gmail.com>
2026-02-04 14:12:53 +0100

    9-6 inheritence exercice

---
+++ chapter9/ice_crean_stand.py
@@ -0,0 +1,15 @@
+from classes import Restaurant
+
+
+class IceCreamStand(Restaurant):
+    def __init__(self, restaurant_name, cuisine_type):
+        super().__init__(restaurant_name, cuisine_type)
+        self.flavors = ["choco", "pstaccia", "vanilla", "raseberry"]
+
+    def display_flavors(self):
+        print(f"flavors :{self.flavors}")
+
+
+creamy = IceCreamStand("IbiCream", "Patesserie")
+
+creamy.display_flavors()
