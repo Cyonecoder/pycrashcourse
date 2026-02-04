a03767ca7f59e86a66126f06b0f680a692b9525d
Cyonecoder <khassal.mohamed1@gmail.com>
2026-02-04 20:07:47 +0100

    finish importing

--- chapter9/my_elictrical_car.py
+++ chapter9/my_elictrical_car.py
@@ -1,13 +1,20 @@
-from car import Car, ElectricCar
+# from car import Car, ElectricCar
+import car
 
-my_tesla = ElectricCar("tesla", "model s", 2019)
+# my_tesla = ElectricCar("tesla", "model s", 2019)
+# print(my_tesla.get_descriptive_name())
+
+# my_beetle = Car("volkswagen", "beetle", 2019)
+# print(my_beetle.get_descriptive_name())
+
+# my_tesla = ElectricCar("tesla", "roadster", 2020)
+
+# print(my_tesla.get_descriptive_name())
+# my_tesla.battery.describe_battery()
+# my_tesla.battery.get_range()
+
+
+my_tesla = car.Car("tesla", "roadster", 2019)
 print(my_tesla.get_descriptive_name())
-
-my_beetle = Car("volkswagen", "beetle", 2019)
-print(my_beetle.get_descriptive_name())
-
-my_tesla = ElectricCar("tesla", "roadster", 2020)
-
-print(my_tesla.get_descriptive_name())
-my_tesla.battery.describe_battery()
-my_tesla.battery.get_range()
+my_mercedes = car.Car("Mercdes", "Gle", 2026)
+print(my_mercedes.get_descriptive_name())
