5ea208a2309b92ed213bdac40ae852db903a9a3c
Mohamed Khassal <khassal.mohamed1@gmail.com>
2026-02-04 14:20:02 +0100

    9-6 inheritence exercice

--- chapter9/ice_crean_stand.py
+++ chapter9/ice_crean_stand.py
@@ -1,4 +1,5 @@
 from classes import Restaurant
+from classes import User
 
 
 class IceCreamStand(Restaurant):
@@ -13,3 +14,17 @@ class IceCreamStand(Restaurant):
 creamy = IceCreamStand("IbiCream", "Patesserie")
 
 creamy.display_flavors()
+
+
+class Admin(User):
+
+    def __init__(self, first_name, last_name, email, date_of_birth, location):
+        super().__init__(first_name, last_name, email, date_of_birth, location)
+        self.privileges = ["add post", "can delete post", "can ban user"]
+
+    def show_privileges(self):
+        print(f"User {self.first_name} has the privileges :{self.privileges}")
+
+
+useradmin = Admin("Chams", "Kilowa", "chams@kilo.com", 1999, "tiznit")
+useradmin.show_privileges()
