56fe95410b87c20c175b33ddf38bd9925f19005b
Mohamed Khassal <khassal.mohamed1@gmail.com>
2026-02-06 15:26:31 +0100

    10-5

--- chapter10/tryityourself/user_name_prompt.py
+++ chapter10/tryityourself/user_name_prompt.py
@@ -5,15 +5,27 @@
 #     name = input("Enter the guest name:")
 #     file_object.write(f"{name}\n")
 
-filenamebook = "guest_book.txt"
+# filenamebook = "guest_book.txt"
 
 
+# f = True
+# while f:
+#     with open(file=filenamebook, mode="+a") as file_object:
+#         name = input("Enter the guest name:")
+#         if name != "q":
+#             print(f"Hello, {name}")
+#             file_object.write(f"{name}\n")
+#         else:
+#             f = False
+# filenamebook = "guest_book.txt"
+
+fileresponses = "programming_responses.txt"
 f = True
 while f:
-    with open(file=filenamebook, mode="+a") as file_object:
-        name = input("Enter the guest name:")
+    with open(file=fileresponses, mode="+a") as file_object:
+        name = input("For what reason you like programming:")
         if name != "q":
-            print(f"Hello, {name}")
+
             file_object.write(f"{name}\n")
         else:
             f = False
