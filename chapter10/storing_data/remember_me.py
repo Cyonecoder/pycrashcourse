97e74f14c69d5ed21812194f7f9022e871979b22
Cyonecoder <khassal.mohamed1@gmail.com>
2026-02-08 16:51:07 +0100

    refactoring 1

--- chapter10/storing_data/remember_me.py
+++ chapter10/storing_data/remember_me.py
@@ -1,8 +1,25 @@
 import json
 
 
-username = input("Whats your name? ")
-filename = "username.json"
-with open(file=filename, mode="w") as f:
-    json.dump(username, f)
-    print(f"We will remember you when you comeback,{username}! ")
+# username = input("Whats your name? ")
+# filename = "username.json"
+# with open(file=filename, mode="w") as f:
+#     json.dump(username, f)
+#     print(f"We will remember you when you comeback,{username}! ")
+
+
+def greet_user():
+    filename = "username.json"
+    try:
+        with open(file=filename) as f:
+            username = json.load(f)
+    except FileNotFoundError:
+        username = input("What is your name? ")
+        with open(file=filename, mode="w") as f:
+            json.dump(username, f)
+            print(f"We will remember you when you comeback,{username}! ")
+    else:
+        print(f"Welcome back, {username}!")
+
+
+greet_user()
