0277d19d242dff9dcc68c11ddc1ae6a06a8402a0
Cyonecoder <khassal.mohamed1@gmail.com>
2026-02-11 21:09:33 +0100

    game runned

--- part_II/alien_invasion/alien_invasion.py
+++ part_II/alien_invasion/alien_invasion.py
@@ -11,3 +11,20 @@ class AlienInvasion:
         pygame.init()
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("alien Invasion")
+
+    def run_game(self):
+        while True:
+            # Watch for keyboard and mouse event
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # make the most recently drawn screen visible
+            pygame.display.flip()
+
+
+if __name__ == "__main__":
+
+    # Make a game instance and run the game
+    ai = AlienInvasion()
+    ai.run_game()
