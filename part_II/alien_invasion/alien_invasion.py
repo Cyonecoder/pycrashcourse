41dbbf2766d35d69bbfb1326b91cef0e3a40d822
Mohamed Khassal <khassal.mohamed1@gmail.com>
2026-02-14 23:16:03 +0100

    deleting bulets

--- part_II/alien_invasion/alien_invasion.py
+++ part_II/alien_invasion/alien_invasion.py
@@ -28,6 +28,10 @@ class AlienInvasion:
             self.ship.update()
             self.bullets.update()
             # make the most recently drawn screen visible
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <= 0:
+                    self.bullets.remove(bullet)
+            print(len(self.bullets))
             self._update_screen()
 
     def _check_events(self):
