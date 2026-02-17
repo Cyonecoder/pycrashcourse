eb84ba68dc847788e871ac136b14b08a633ae5f6
Mohamed Khassal <khassal.mohamed1@gmail.com>
2026-02-17 20:20:38 +0000

    render one topic

--- part_II/project_3_django/learning_log/learning_logs/views.py
+++ part_II/project_3_django/learning_log/learning_logs/views.py
@@ -19,7 +19,7 @@ def topics(request):
 
 def topic(request, topic_id):
     """show a single topic and all its entries"""
-    topic = Topic.objects.get(topic_id)
+    topic = Topic.objects.get(id=topic_id)
     entries = topic.entry_set.order_by("-date_add")
     context = {"topic": topic, "entries": entries}
     return render(request, "learning_logs/topic.html", context)
