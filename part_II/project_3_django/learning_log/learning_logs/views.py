bd96679e919103925f38ef17e49ece90ca66abf1
Mohamed Khassal <khassal.mohamed1@gmail.com>
2026-02-23 21:34:22 +0000

    check topics only if user is logged in

--- part_II/project_3_django/learning_log/learning_logs/views.py
+++ part_II/project_3_django/learning_log/learning_logs/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from .models import Topic, Entry
 
 from .forms import TopicForm, EntryForm
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -11,6 +12,7 @@ def index(request):
     return render(request, "learning_logs/index.html")
 
 
+@login_required
 def topics(request):
     """show all topics"""
 
