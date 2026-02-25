8d8d40bcf4adacab6011d4f8555a116615479e7c
Mohamed Khassal <khassal.mohamed1@gmail.com>
2026-02-25 11:19:04 +0000

    associate new topics with the user

--- part_II/project_3_django/learning_log/learning_logs/views.py
+++ part_II/project_3_django/learning_log/learning_logs/views.py
@@ -5,6 +5,8 @@ from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 from django.contrib.auth.decorators import login_required
 
+from django.http import Http404
+
 
 # Create your views here.
 def index(request):
@@ -16,7 +18,7 @@ def index(request):
 def topics(request):
     """show all topics"""
 
-    topics = Topic.objects.order_by("date_add")
+    topics = Topic.objects.filter(owner=request.user).order_by("date_add")
     context = {"topics": topics}
     return render(request, "learning_logs/topics.html", context)
 
@@ -25,6 +27,8 @@ def topics(request):
 def topic(request, topic_id):
     """show a single topic and all its entries"""
     topic = Topic.objects.get(id=topic_id)
+    if topic.owner != request.user:
+        raise Http404
     entries = topic.entry_set.order_by("-date_add")
     context = {"topic": topic, "entries": entries}
     return render(request, "learning_logs/topic.html", context)
@@ -38,7 +42,9 @@ def new_topic(request):
     else:
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect("learning_logs:topics")
 
     context = {"form": form}
@@ -67,7 +73,8 @@ def edit_entry(request, entry_id):
 
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
-
+    if topic.owner != request.user:
+        raise Http404
     if request.method != "POST":
         # Initial request; prefill form with the curent entry.
         form = EntryForm(instance=entry)
